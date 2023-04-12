cddec0b0de33bc9eea5ec18ddb2092fc2ac84e24
Lelouch <99410452+Impure-King@users.noreply.github.com>
2023-04-12 06:11:12 -0600

    my current website

---
+++ templates/script/base.ts
@@ -0,0 +1,11 @@
+const button_toggler = document.getElementById("toggler");
+
+var toggle = 0;
+
+button_toggler?.addEventListener('click', () => {
+    if (toggle % 2 == 0){
+        alert("The navbar was toggled");
+        console.log("lol")
+    }
+    toggle += 1;
+});
