18bdbcb564450c522677acbfbdf009075f4944e0
Lelouch <99410452+Impure-King@users.noreply.github.com>
2023-11-22 19:21:10 -0700

    Made some changes

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
 import numpy as np
-
+import matplotlib.pyplot as plt
+import matplotlib
+import os
+matplotlib.use("agg")
 app = Flask(__name__)
 
-answer_page = "current_answer"
+answer_page = "answer"
 
 # An Error handling class:
 class IncorrectInput(object):
@@ -13,6 +16,43 @@ class IncorrectInput(object):
     def __init__(self, error: str) -> None:
         self.error_message = error
 
+# Various helper functions:
+def equation_definer(h:float, k:float, a:float):
+    lists = [h, k, a]
+    for index, i in enumerate(lists):
+        if i == int(i):
+            lists[index] = int(i)
+    h, k, a = lists
+    part1 = "y = "
+    part2 = "(x - h)^2"
+    part3 = " + k"
+    if h == 0:
+        part2 = "x^2"
+    elif h < 0:
+        part2 = f"(x + {(h**2)**0.5})^2"
+    else:
+        part2 = f"(x - {h})^2"
+    
+    if a == 0:
+        part2 = ""
+    elif a != 1:
+        part2 = str(a) + part2
+    
+    if k == 0:
+        part3 = ""
+    elif k < 0:
+        part3 = f" - {(k**2)**0.5}"
+    else:
+        part3 = f" + {k}"
+
+
+    # Handling the graphing:
+    x = np.arange(-100, 100)
+    y = a * (x - h)**2 + k
+    plt.plot(x, y)
+    plt.savefig("./static/images/plot.jpg")
+    plt.clf()
+    return part1 + part2 + part3
 
 def convert(number:str):
     """Converts a string to a float."""
@@ -30,23 +70,25 @@ def start():
 # Actual Project input page:
 @app.route('/Quadratic Regressor')
 def project():
-    return render_template("project1.html", answer_page=answer_page)
+    return render_template("project1.html", answer_page=answer_page, plot_show=False)
 
 # Solved page:
 @app.route('/' + answer_page, methods=["POST"])
-def solve():
-    valid_input = True
+def solve1():
     h = convert('vertexx')
     k = convert('vertexy')
     x = convert('pointx')
     y = convert('pointy')
     a = vertex_solver(h, k, x, y)
-    if isinstance(a, IncorrectInput):
-        valid_input=False
-        equation=a.error_message
-    else:
-        equation = f"y = {a}(x - {h})^2 + {k}"
-    return render_template("answer1.html", equation=equation, valid_input=valid_input)
+    equation = equation_definer(h, k, a)
+    return render_template("project1.html", equation=equation, plot_show=True)
+
+@app.route('/answer2', methods=["POST"])
+def solve2():
+    x1, y1 = convert("x1"), convert("y1")
+    x2, y2 = convert("x2"), convert("y2")
+    x3, y3 = convert("x3"), convert("y3")
+    return render_template("project1.html", equation=1)
 
 # Error Handlers:
 @app.errorhandler(404)
--- static/scripts/validate_form.js
+++ static/scripts/validate_form.js
@@ -1,18 +1,58 @@
 // Ensuring the DOM content has loaded
-function validateNonEmpty() {
+function validateNonEmpty2() {
+    var x1 = document.forms['solver_2']["x1"].value;
+    var y1 = document.forms["solver_2"]["y1"].value;
+    var x2 = document.forms['solver_2']["x2"].value;
+    var y2 = document.forms["solver_2"]["y2"].value;
+    var x3 = document.forms['solver_2']["x3"].value;
+    var y3 = document.forms["solver_2"]["y3"].value;
+    
+    if (x1 == "" || x2 == "" || x3 == "" || y1 == "" || y2 == "" || y3 == "") {
+        alert("One of the coordinates have not been filled out.");
+        return false;
+    }
+    obj = document.getElementById("solver_1");
+    obj.style.display = "none";
+
+}
+
+function validateNonEmpty1() {
     var vertexx = document.forms['solver_1']["vertexx"].value;
     var vertexy = document.forms["solver_1"]["vertexy"].value;
     var pointx = document.forms['solver_1']["pointx"].value;
     var pointy = document.forms["solver_1"]["pointy"].value;
 
     if (vertexx == "" || vertexy == "" || pointx == "" || pointy == "") {
-        alert("One of the points have not been filled out.");
+        alert("One of the coordinates have not been filled out.");
         return false;
     }
-
-    else if (parseFloat(vertexx) == parseFloat(pointx)) {
-        alert("The vertex and the point can't have the same x-coordinate.");
-        return false;
+    else{
+        if (parseFloat(vertexy) == parseFloat(pointy) && parseFloat(pointx) == parseFloat(vertexx)) {
+            alert("These are coincidental points.");
+            return false;
+        }
+        if (parseFloat(vertexx) == parseFloat(pointx)) {
+            alert("The vertex and the point can't have the same x-coordinate."); // Add explaination.
+            return false;
+        }
+        if (parseFloat(vertexy) == parseFloat(pointy)) {
+            alert("The vertex and the point can't have the same y-coordinate.");
+            return false;
+        }
     }
 
+}
+
+function changeForm() {
+    var method_name = document.getElementById("methods").value;
+    const method1 = document.getElementById("solver_1");
+    const method2 = document.getElementById("solver_2");
+    method1.style.display = "none";
+    method2.style.display = "none";
+    if (parseInt(method_name) === 1) {
+        method1.style.display = "block";
+    }
+    else if (parseInt(method_name) === 2) {
+        method2.style.display = "block";
+    }
 }
